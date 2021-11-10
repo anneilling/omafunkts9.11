1e29293606c58284cfcd3721592d5a8fd2b5dd9c
opilane <opilane@HP-CZC6297BBL>
2021-11-10 12:29:07 +0200

    10.11

--- module1.py
+++ module1.py
@@ -10,3 +10,43 @@ def arithmetic(a:float,b:float,c:str):
     :rtype float:
     """
 
+    if c=="+":
+        n=a+b
+    elif c=="-":
+        n=a-b
+    elif c=="*":
+        n=a*b
+    elif c=="/":
+        if b!=0:
+            n=a/b
+        else:
+            print("Div0")
+            r=0.0
+    else:
+        print("Viga!")
+    return n
+
+def is_year_leap(aasta: int):
+
+    #Liigaasta leidmine
+    #Tagastab True kui on liigaasta ja False kui ei ole
+    #:param int aasta:Asta number
+    #:rtype bool:Funktsioni vastus logilises formatis
+   
+    if aasta%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vastus
+
+from math import sqrt
+def is_prime(number):
+    if number % 2 == 0 and number != 2:
+        return False
+    if number == 0 or number == 1:
+        return False
+    for n in range(3, int(sqrt(number).real) + 1, 2):
+        if number % n == 0:  
+            return False
+    return True  
+
--- omafunkts9.11.py
+++ omafunkts9.11.py
@@ -1,5 +1,19 @@
-while true:
+from module1 import *
+from math import *
+
+while True:
     print("Funktsioonid" .center(50,"+"))
-    v=input("arithmetic - A")
-    if v-upper()=="A":
-        arithmetic
+    v=input("arithmetic - A,\nis_year_leap-Y\nis_prime-N")
+    v=input()
+    if v.upper()=="A":
+        arv1=float(input("Arv 1:"))
+        arv2=float(input("Arv 2:"))
+        sym=input("Tehe: ")
+        tulemus=arithmetic(arv1,arv2,sym)
+        print(tulemus)
+    elif v.upper()=="Y":
+        tulemus=is_year_leap(int(input("Sisesta aasta ")))
+        print(tulemus)
+    elif v.upper()=="N":
+        tulemus=is_prime(int(input("Sisesta number ")))
+        print(tulemus)
