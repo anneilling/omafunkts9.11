f0406539a1cf27b77227bde156b0b3c312e56c2c
opilane <opilane@HP-CZC6297BBL>
2021-11-10 11:19:49 +0200

    Add project files.

---
+++ module1.py
@@ -0,0 +1,12 @@
+def arithmetic(a:float,b:float,c:str):
+    """Lihtne kalkulaator
+    +- liitmine
+    --lahutamine
+    *-korrutamine
+    /-jagamine
+    :param float a:Esimene arv
+    :param float b:Teine arv
+    :param str c:Aritmeetiline tehing
+    :rtype float:
+    """
+
---
+++ omafunkts9.11.py
@@ -0,0 +1,5 @@
+while true:
+    print("Funktsioonid" .center(50,"+"))
+    v=input("arithmetic - A")
+    if v-upper()=="A":
+        arithmetic
